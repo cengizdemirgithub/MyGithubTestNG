4171cd8bac9e43925643c7cbd03fae2bcd505598
Cengiz Demir <kontakt.cengiz.demir@gmail.com>
2022-06-25 22:49:38 +0200

    25.06.2022

--- src/test/java/tests/Test03.java
+++ src/test/java/tests/Test03.java
@@ -1,5 +1,6 @@
 package tests;
 
+import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.testng.annotations.Test;
@@ -14,9 +15,17 @@ public class Test03 extends TestBase {
     //test data password : manager1!
     //Degerleri girildiginde sayfaya girilemedigini test et
     @Test
-    public void negativeTest001(){
+    public void negativeTest001() {
         driver.get("https://www.hotelmycamp.com/ ");
-        WebElement loginButton=driver.findElement(By.xpath("//*[text()='Log in']"));
+        WebElement loginButton = driver.findElement(By.xpath("//*[text()='Log in']"));
         loginButton.click();
+        WebElement username = driver.findElement(By.id("UserName"));
+        username.sendKeys("manager1");
+        WebElement password = driver.findElement(By.id("Password"));
+        password.sendKeys("manager1!");
+        WebElement submitButton = driver.findElement(By.id("btnSubmit"));
+        submitButton.click();
+        Assert.assertTrue(driver.findElement(By.xpath("//*[text()='Try again please']")).isDisplayed());
+
     }
 }
