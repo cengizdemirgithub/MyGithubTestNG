be1fbd8d89c5865d280291b85b143868b8656aa6
Cengiz Demir <kontakt.cengiz.demir@gmail.com>
2022-07-05 00:36:52 +0200

    05.07.2022

--- src/test/java/tests/Test04.java
+++ src/test/java/tests/Test04.java
@@ -5,14 +5,17 @@ import org.junit.Assert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
 import org.testng.annotations.Test;
 import utilities.TestBase;
+
+import java.util.Random;
 
 import static org.bouncycastle.cms.RecipientId.password;
 
 public class Test04 extends TestBase {
     @Test
-    public void test01(){
+    public void test01() {
         //1. Launch browser
         //2. Navigate to url 'http://automationexercise.com'
         driver.get("http://automationexercise.com");
@@ -22,39 +25,47 @@ public class Test04 extends TestBase {
         Assert.assertTrue(driver.getTitle().contains("Automation"));
 
         //4. Add products to cart
-        WebElement product1=driver.findElement(By.xpath("//*[@data-product-id='1']"));
+        WebElement product1 = driver.findElement(By.xpath("//*[@data-product-id='1']"));
         product1.click();
 
         //5. Click 'Cart' button
         WebElement contuniueShopping = driver.findElement(By.xpath("//*[text()='Continue Shopping']"));
         contuniueShopping.click();
-        WebElement chart1=driver.findElement(By.xpath("//*[text()=' Cart']"));
+        WebElement chart1 = driver.findElement(By.xpath("//*[text()=' Cart']"));
         chart1.click();
         //6. Verify that cart page is displayed
-        String cartPage="https://automationexercise.com/view_cart";
+        String cartPage = "https://automationexercise.com/view_cart";
         Assert.assertTrue(driver.getCurrentUrl().equals(cartPage));
         //7. Click Proceed To Checkout
-        WebElement checkout=driver.findElement(By.xpath("//*[text()='Proceed To Checkout']"));
+        WebElement checkout = driver.findElement(By.xpath("//*[text()='Proceed To Checkout']"));
         checkout.click();
         //8. Click 'Register / Login' button
-        WebElement register=driver.findElement(By.xpath("(//a[@href='/login'])[2]"));
+        WebElement register = driver.findElement(By.xpath("(//a[@href='/login'])[2]"));
         register.click();
-        
+
         //9. Fill all details in Signup and create account
-        Faker faker=new Faker();
-       // WebElement eMail=driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/input[2]"));
-      //  eMail.sendKeys(faker.internet().emailAddress());
-       // WebElement password=driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/input[3]"));
-      //  password.sendKeys(faker.internet().password());
+        Faker faker = new Faker();
+        // WebElement eMail=driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/input[2]"));
+        //  eMail.sendKeys(faker.internet().emailAddress());
+        // WebElement password=driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[1]/div/form/input[3]"));
+        //  password.sendKeys(faker.internet().password());
         WebElement name = driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/form/input[2]"));
         name.sendKeys(faker.name().fullName());
-        WebElement eMail2=driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/form/input[3]"));
+        WebElement eMail2 = driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/form/input[3]"));
         eMail2.sendKeys(faker.internet().emailAddress());
         WebElement signUpTest = driver.findElement(By.xpath("//*[@id=\"form\"]/div/div/div[3]/div/form/button"));
         signUpTest.sendKeys(Keys.ENTER);
+        WebElement gender = driver.findElement(By.id("id_gender1"));
+        gender.click();
+        WebElement gender2 = driver.findElement(By.id("id_gender2"));
+        gender2.click();
+        WebElement password = driver.findElement(By.id("password"));
+        password.sendKeys(faker.internet().password());
+        Select select = new Select(driver.findElement(By.id("days")));
+        Random random = new Random();
 
-
-
+        select.selectByVisibleText(random.ints().toString());
+        driver.findElement(By.id("days")).click();
 
 
         //10. Verify 'ACCOUNT CREATED!' and click 'Continue' button
