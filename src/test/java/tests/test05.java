d192d7fcd79f0b3060f577e151de32b9bdd6b2e9
Cengiz Demir <kontakt.cengiz.demir@gmail.com>
2022-07-27 23:59:21 +0200

    27.07.2022

--- src/test/java/tests/test05.java
+++ src/test/java/tests/test05.java
@@ -7,19 +7,37 @@ import java.sql.Driver;
 
 public class test05 extends TestBase {
     @Test
-    public void testCase01() {
+    public void testCase01() throws InterruptedException {
         //1. Launch browser
         //2. Navigate to url 'http://automationexercise.com'
-        //driver.get("http://automationexercise.com");
+
+
+
+        //foo(null);
         try {
-            driver.navigate().to("http://automationexercise.com");
-        } catch (Exception e) {
-            e.printStackTrace();
+            driver.get("http://automationexercise.com");
+        } catch (NullPointerException e) {
+            System.out.println("NullPointerException thrown!");
+            if(obj == null) {
+                // Do something
+            } else {
+                // Do something else
+            }
+
         }
+
+
         //3. Verify that home page is visible successfully
         //4. Scroll down page to bottom
         //5. Verify 'SUBSCRIPTION' is visible
         //6. Click on arrow at bottom right side to move upward
         //7. Verify that page is scrolled up and 'Full-Fledged practice website for Automation Engineers' text is visible on screen
+    }
+    public <driver> void doSomething(driver obj) {
+        if(obj == null) {
+            // Do something
+        } else {
+            // Do something else
+        }
     }
 }
