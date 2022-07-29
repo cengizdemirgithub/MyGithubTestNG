01be3cd571dad8087a3d1d495c73045eae6fedd3
Cengiz Demir <kontakt.cengiz.demir@gmail.com>
2022-07-29 23:57:18 +0200

    29.07.2022

--- src/test/java/tests/test05.java
+++ src/test/java/tests/test05.java
@@ -18,7 +18,7 @@ import static java.awt.SystemColor.window;
 
 public class test05 extends TestBase {
     @Test
-    public void testCase01() throws IOException{
+    public void testCase01() throws IOException, InterruptedException {
         //1. Launch browser
         //2. Navigate to url 'http://automationexercise.com';
         driver.get("http://automationexercise.com");
@@ -26,12 +26,17 @@ public class test05 extends TestBase {
         Assert.assertFalse(driver.getTitle().isEmpty());
         //4. Scroll down page to bottom
         JavascriptExecutor jse = (JavascriptExecutor)driver;
-        jse.executeScript("window.scrollBy(0,250)");
+        jse.executeScript("window.scrollTo(0, document.body.scrollHeight)");
+
+        Thread.sleep(3000);
 
         //5. Verify 'SUBSCRIPTION' is visible
         Assert.assertTrue(driver.findElement(By.xpath("//*[text()='Subscription']")).isDisplayed());
         //6. Click on arrow at bottom right side to move upward
+        WebElement scrolled = driver.findElement(By.id("scrollUp"));
+        scrolled.click();
         //7. Verify that page is scrolled up and 'Full-Fledged practice website for Automation Engineers' text is visible on screen
+        Assert.assertTrue(driver.findElement(By.xpath("//*[text()='Full-Fledged practice website for Automation Engineers']")).isDisplayed());
     }
 
 
