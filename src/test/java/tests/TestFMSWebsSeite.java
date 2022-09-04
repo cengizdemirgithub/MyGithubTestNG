f526612fff251aa3ec63a1856a4eab5a7b0671db
Cengiz Demir <kontakt.cengiz.demir@gmail.com>
2022-09-04 23:53:39 +0200

    -____-

--- src/test/java/tests/TestFMSWebsSeite.java
+++ src/test/java/tests/TestFMSWebsSeite.java
@@ -1,5 +1,6 @@
 package tests;
 
+import com.github.javafaker.Faker;
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.junit.Assert;
 
@@ -18,15 +19,16 @@ import java.sql.Driver;
 
 public class TestFMSWebsSeite extends TestBase {
     JavascriptExecutor js = (JavascriptExecutor) driver;
-
+    Faker fake = new Faker();
 
 
     //2) https://www.future-mobility-solutions.com/
     @BeforeMethod
-    public void beforeClassMethod(){
+    public void beforeClassMethod() {
         //driver.get("https://www.future-mobility-solutions.com/");
         driver.navigate().to("https://www.future-mobility-solutions.com/");
     }
+
     @Test
     public void test01_Bewerbung() throws InterruptedException {
 
@@ -44,12 +46,38 @@ public class TestFMSWebsSeite extends TestBase {
 
     }
 
+    @Test
+    public void test02_Kontakt() throws InterruptedException {
+        driver.findElement(By.xpath("//*[@id='uc-btn-accept-banner']")).click();//cookie web element
+        Thread.sleep(2000);
+        WebElement kontakt = driver.findElement(By.xpath("(//*[text()='Kontakt'])[1]"));
+        kontakt.click();
+        Thread.sleep(2000);
+        WebElement name = driver.findElement(By.xpath("//*[@placeholder='Name*']"));
+        name.sendKeys(fake.name().fullName());
+        Thread.sleep(2000);
+        WebElement email = driver.findElement(By.xpath("//*[@placeholder='E-Mail-Adresse*']"));
+        email.sendKeys(fake.internet().emailAddress());
+        Thread.sleep(2000);
+        WebElement telNumber = driver.findElement(By.xpath("//*[@placeholder='Telefonnummer']"));
+        telNumber.sendKeys(fake.phoneNumber().cellPhone());
+        Thread.sleep(2000);
+        WebElement message = driver.findElement(By.xpath("//*[@placeholder='Freitextfeld*']"));
+        message.sendKeys("calm down only a trying:)))");
+        Thread.sleep(2000);
+        WebElement checkbox = driver.findElement(By.xpath("(//input[@type='checkbox'])[1]"));
+        checkbox.click();
+        WebElement question = driver.findElement(By.xpath("//*[@name='random-math-quiz']"));
+        question.click();
+        question.sendKeys("3");
+        WebElement submit = driver.findElement(By.xpath("(//*[@type='submit'])[1]"));
+        submit.submit();
+
+
+    }
 
 
 }
-
-
-
 
 
 
